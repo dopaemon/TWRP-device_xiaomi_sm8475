5ff028f51877ed2403a07deafa4448b22b1a98a5
dopaemon <polarisdp@gmail.com>
2024-07-04 12:25:18 +0700

    mayfly: Add vendorsetup
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,7 +174,7 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_MAX_BRIGHTNESS := 2047
 ifeq ($(TW_DEFAULT_LANGUAGE),)
-TW_DEFAULT_LANGUAGE := zh_CN
+TW_DEFAULT_LANGUAGE := en_US
 endif
 TW_DEFAULT_BRIGHTNESS := 200
 TWRP_INCLUDE_LOGCAT := true
